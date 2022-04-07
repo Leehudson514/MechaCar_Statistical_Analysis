95954e7c7dfe2604dd710065996269bd9148dfa3
Lee Hudson <leehudson514@gmail.com>
2022-04-07 11:56:10 -0700

    challenge

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+# Challenge 1
+#read csv mpg data
+mpg_data <- read.csv("C:/Users/leehu/Desktop/Berkeley/MechaCar_Statistical_Analysis/Resources/MechaCar_mpg.csv") #import dataset
+
+#import dplyr library
+library(dplyr)
+
+# lm function for linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data)
+
+# summary function to determine p-value & r-squared value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data)) 
+
+# Challenge 2
